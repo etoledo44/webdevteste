8d2be418c54521e4acf065ecfcb4886ce7a8e4d4
Érique Toledo <42679155+etoledo44@users.noreply.github.com>
2025-02-24 11:36:47 -0300

    aa
    aa

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <div>
+          <h1>Portfolio</h1>
+          <div>
+          <p>Erique Toledo</p>
+
+          </div>
+          <img style={{width: '50%', borderRadius: '100%'}} src='https://avatars.githubusercontent.com/u/42679155?v=4' />
+          <p>Projetos</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
